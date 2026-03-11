b0bd94c7ebc79549468209b0188a5db963f02018
Tobiach <esenciezen@gmail.com>
2026-03-11 20:37:29 -0300

    Clean repo

--- EXTRAIDO 2.0---v1---mvp-encasa-venezuela---diseño-(09_02_26)/assets/hero/README.md
+++
@@ -1,6 +0,0 @@
-# Hero Images Directory
-Place your carousel images here.
-Recommended names:
-- banner_1.png
-- banner_2.png
-- banner_3.png
--- EXTRAIDO 2.0---v1---mvp-encasa-venezuela---diseño-(09_02_26)/components/CoverageMap.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const CoverageMap: React.FC = () => {
-  return null; // Integrated into Testimonials for layout fidelity
-};
-
-export default CoverageMap;
